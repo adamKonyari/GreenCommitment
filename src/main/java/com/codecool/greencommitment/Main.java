806801d75932f4cbce48a035d13409cd12ebebba
adamKonyari <adam.konyari@gmail.com>
2019-03-11 06:59:07 +0100

    main method update

---
+++ src/main/java/com/codecool/greencommitment/Main.java
@@ -0,0 +1,17 @@
+package com.codecool.greencommitment;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        if(args[0].equals("CLIENT") ) {
+            // STARTING CLIENT CODE
+        } else if (args[1].equals("SERVER")) {
+            // STARTING SERVER CODE
+        } else {
+            System.out.println("Invalid argument!");
+            System.out.println("Possible arguments: CLIENT, SERVER");
+        }
+
+    }
+}
